a3ee354d7235a0b99581aa4d974aa2d3f6874889
Heiko Thiery <heiko.thiery@gmail.com>
2020-01-04 20:36:14 +0100

    gravatar: make python2/3 compatible
    Signed-off-by: Heiko Thiery <heiko.thiery@gmail.com>

--- app/gravatar.py
+++ app/gravatar.py
@@ -1,6 +1,9 @@
 import re
+import sys
 import urllib, hashlib
-import urllib.parse
+
+if int(sys.version[0]) > 2:
+    import urllib.parse
 
 
 def get_gravatar_link(developer, size=30):
@@ -11,8 +14,13 @@ def get_gravatar_link(developer, size=30):
     if match:
         email = match.group(1)
 
-    url = "https://www.gravatar.com/avatar/" + hashlib.md5(email.lower().encode('utf-8')).hexdigest() + "?"
-    url += urllib.parse.urlencode({'d':default, 's':str(size)})
+    if int(sys.version[0]) > 2:
+        url = "https://www.gravatar.com/avatar/" + hashlib.md5(email.lower().encode('utf-8')).hexdigest() + "?"
+        url += urllib.parse.urlencode({'d':default, 's':str(size)})
+    else:
+        url = "https://www.gravatar.com/avatar/" + hashlib.md5(email.lower()).hexdigest() + "?"
+        url += urllib.urlencode({'d':default, 's':str(size)})
+
     return url
 
 
